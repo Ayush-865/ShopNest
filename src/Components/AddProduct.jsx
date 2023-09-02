3b87f5227381ac13fb007eaa08f77295cb8d9322
Ayush <ayush.up105@gmail.com>
2023-09-03 02:39:51 +0530

    error handling

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -12,6 +12,7 @@ const AddProduct = () => {
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
   const id = JSON.parse(localStorage.getItem('user')).authToken;
+  const [error, setError]=useState(false);
 
   const navigate = useNavigate();
 
@@ -36,7 +37,12 @@ const AddProduct = () => {
   }
 
   const handleAddProduct = async () => {
+    if(!name || !price || !category || !company){
+      setError(true)
+      return false;
+    }
     try {
+      setIsLoading(true);
       const res = await fetch("https://shopnest-backend.onrender.com/api/products/addProduct", {
       mode: 'cors',
       headers: {
@@ -59,7 +65,6 @@ const AddProduct = () => {
       setIsLoading(false);
     }
   }
-
   return (
     <>
       {
@@ -71,10 +76,10 @@ const AddProduct = () => {
           <h1>Add Product</h1>
           <img src={productImage} alt='Profile Image' />
           <input className='target' accept='image/*' type='file' onChange={getProductImg} />
-          <input id='one' className='inputBox' type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter Name' autocomplete="off" />
-          <input id='two' className='inputBox' type='text' value={price} onChange={(e) => setPrice(e.target.value)} placeholder='Enter Price' autocomplete="off"/>
-          <input id='three' className='inputBox' type='text' value={category} onChange={(e) => setCategory(e.target.value)} placeholder='Enter Category' autocomplete="off"/>
-          <input id='four' className='inputBox' type='text' value={company} onChange={(e) => setCompany(e.target.value)} placeholder='Enter Company' autocomplete="off"/>
+          <input id='one' className={`${error && !name ? 'redplaceholder inputBox' : 'inputBox'}`} type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter Name' autocomplete="off" />
+          <input id='two' className={`${error && !price ? 'redplaceholder inputBox' : 'inputBox'}`} type='text' value={price} onChange={(e) => setPrice(e.target.value)} placeholder='Enter Price' autocomplete="off"/>
+          <input id='three' className={`${error && !category ? 'redplaceholder inputBox' : 'inputBox'}`} type='text' value={category} onChange={(e) => setCategory(e.target.value)} placeholder='Enter Category' autocomplete="off"/>
+          <input id='four' className={`${error && !company ? 'redplaceholder inputBox' : 'inputBox'}`} type='text' value={company} onChange={(e) => setCompany(e.target.value)} placeholder='Enter Company' autocomplete="off"/>
           <button className='loginbutton' type='button' onClick={handleAddProduct}>Add</button>
         </div>
       </div>
